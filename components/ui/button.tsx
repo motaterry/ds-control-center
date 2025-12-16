fec7df926af0ebabbc8b9421890a2c37b2c6b105
motaterry <terrymota0@gmail.com>
2025-12-16 02:56:53 -0300

    fix: Ensure Button icon size variant types are properly inferred

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -23,7 +23,7 @@ const buttonVariants = cva(
         sm: "h-9 px-3",
         lg: "h-11 px-8",
         icon: "h-10 w-10",
-      },
+      } as const,
     },
     defaultVariants: {
       variant: "default",
